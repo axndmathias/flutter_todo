aa522ebd8c5af3135bf05331e83155225faee71e
Alexandre Mathias <mathias.alexandre@gmail.com>
2024-02-21 11:25:16 +0100

    Add Routefly

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:routefly/routefly.dart';
+
+import '../routes.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp.router(
+      title: 'Flutter To Do',
+      routerConfig:
+          Routefly.routerConfig(routes: routes, initialPath: routePaths.splash),
+    );
   }
 }
